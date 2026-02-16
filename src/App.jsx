d98e9e76c86eae02397a1b9ad8a2a9e775f2959c
KUSNAM AKHIL REDDY <2310030387@klh.edu.in>
2026-02-16 11:42:38 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css"
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from 'react-router-dom'
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Login" element={<Login />} />
+            </Routes>
+        </Router>
+    )
+}
